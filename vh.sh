71620c6f37341ab6ec9d180b7c4a443929e1384d
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-06-24 04:50:20 +0000

    Done.

--- vh.sh
+++ vh.sh
@@ -11,24 +11,24 @@ mkdir -p /home/vh/$1/public_html
 
 cat <<EOF > /etc/nginx/sites-available/$1
 server {
-	listen 80;
+        listen 80;
         listen [::]:80;
 
-	root /home/vh/$1/public_html;
-	index index.php index.html index.htm;
-	server_name $1 www.$1;
+        root /home/vh/$1/public_html;
+        index index.php index.html index.htm;
+        server_name $1 www.$1;
 
-    location / {
-            try_files $uri $uri/ =404;
-    }
+        #location / {
+        #        try_files $uri $uri/ =404;
+        #}
 
-	location ~ \.php$ {
-        try_files $uri $uri/ =404;
-		fastcgi_split_path_info ^(.+\.php)(/.+)$;
-		fastcgi_pass 127.0.0.1:9000;
-		fastcgi_index index.php;
-		include fastcgi_params;
-	}
+        location ~ \.php$ {
+                #try_files $uri $uri/ =404;
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                fastcgi_pass 127.0.0.1:9000;
+                fastcgi_index index.php;
+                include fastcgi_params;
+        }
 }
 EOF
 
@@ -39,11 +39,16 @@ cat <<EOF > /home/vh/$1/public_html/index.html
 </html>
 EOF
 
+
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
 sudo service nginx reload
+nginx -t
+
 else
     echo "Please Write A Domain Name EX. ./vh.sh mydomain.com"
 fi
 echo "**********************************************************"
+echo " $1 Is Done."
+echo "/etc/nginx/sites-available/$1"
 echo " You Can Upload Your Files Into /home/vh/$1/public_html "
 echo "**********************************************************"
